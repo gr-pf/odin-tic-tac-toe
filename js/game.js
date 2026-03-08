76edd976f647541e0ef6eca851996b7d3068c134
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-08 17:11:53 +0100

    Add playerTurn state

--- js/game.js
+++ js/game.js
@@ -7,16 +7,20 @@ export function Game() {
   this.BoardGame = new BoardGame();
   this.playerOne = null;
   this.playerTwo = null;
+  this.playerTurn = null;
 
   this.initGame = function () {
     this.state = "active";
     this.BoardGame = new BoardGame();
 
+    //Essayer d'introduire une fonction imbriqué pour avoir une fonction asKNameP1 et askNameP2 tiré de la même fonction
     const namePlayerOne = askName();
     const markPlayerOne = askMark();
     this.playerOne = new Player(namePlayerOne, markPlayerOne);
     const namePlayerTwo = askName();
     const markPlayerTwo = markPlayerOne === "X" ? "O" : "X";
     this.playerTwo = new Player(namePlayerTwo, markPlayerTwo);
+
+    this.playerTurn = this.playerOne;
   };
 }
