bf47dba11ada7725b2ef84a72e550ded7a173f93
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-08 17:02:47 +0100

    Start game constructor

--- js/game.js
+++ js/game.js
@@ -1,65 +1,22 @@
+import { BoardGame } from "./boardgame.js";
+import { Player } from "./player.js";
+import { askMark, askName } from "../functions.js";
+
 export function Game() {
-  this.gameBoard = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ];
+  this.state = "active";
+  this.BoardGame = new BoardGame();
+  this.playerOne = null;
+  this.playerTwo = null;
 
-  this.markCounter = 0;
+  this.initGame = function () {
+    this.state = "active";
+    this.BoardGame = new BoardGame();
 
-  this.markValue = {
-    X: 1,
-    O: -1,
-  };
-
-  this.markGrid = function (mark, gridArea) {
-    if (this.gameBoard[gridArea[0]][gridArea[1]] !== "") {
-      console.log("This cell is full. Choose another !");
-      return false;
-    }
-    this.gameBoard[gridArea[0]][gridArea[1]] = this.markValue[mark];
-    this.markCounter++;
-    return true;
-  };
-
-  this.showGameBoard = function () {
-    console.log(this.gameBoard);
-  };
-
-  this.checkState = function () {
-    let res;
-    for (let r = 0; r < 3; r++) {
-      res = 0;
-      for (let c = 0; c < 3; c++) {
-        res += this.gameBoard[r][c];
-      }
-      if (res === 3 || res === -3) {
-        return res;
-      }
-    }
-    for (let c = 0; c < 3; c++) {
-      res = 0;
-      for (let r = 0; r < 3; r++) {
-        res += this.gameBoard[r][c];
-      }
-      if (res === 3 || res === -3) {
-        return res;
-      }
-    }
-    res = 0;
-    for (let i = 0; i < 3; i++) {
-      res += this.gameBoard[i][i];
-      if (res === 3 || res === -3) {
-        return res;
-      }
-    }
-    res = 0;
-    for (let i = 0; i < 3; i++) {
-      res += this.gameBoard[i][2 - i];
-      if (res === 3 || res === -3) {
-        return res;
-      }
-    }
-    return this.markCounter === 9 ? "draw" : "continue";
+    const namePlayerOne = askName();
+    const markPlayerOne = askMark();
+    this.playerOne = new Player(namePlayerOne, mark);
+    const namePlayerTwo = askName();
+    const markPlayerTwo = markPlayerOne === "X" ? "O" : "X";
+    this.playerTwo = new Player(namePlayerTwo, mark);
   };
 }
