97836ac96c160719928b721d5d68759cbf6c48e9
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-09 14:07:06 +0100

    Add playRound

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 import { BoardGame } from "./boardgame.js";
 import { Player } from "./player.js";
-import { askMark, askName } from "../functions.js";
+import { askMark, askName, askMove } from "../functions.js";
 
 export function Game() {
   this.state = "inactive";
@@ -22,5 +22,19 @@ export function Game() {
     this.playerTwo = new Player(namePlayerTwo, markPlayerTwo);
 
     this.playerTurn = this.playerOne;
+  };
+
+  this.playRound = function () {
+    let round = false;
+    while (!round) {
+      const move = askMove();
+      if (this.playerTurn === this.playerOne) {
+        round = this.BoardGame.markGrid(this.playerOne.mark, move);
+      } else {
+        round = this.BoardGame.markGrid(this.playerTwo.mark, move);
+      }
+    }
+    this.playerTurn =
+      this.playerTurn === this.playerOne ? this.playerTwo : this.playerOne;
   };
 }
