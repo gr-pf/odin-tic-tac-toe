cdb0511f9441197a38ff33a9e100054e642abb9f
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-09 14:06:38 +0100

    Add askMove

--- functions.js
+++ functions.js
@@ -10,3 +10,15 @@ export function askMark() {
   }
   return mark;
 }
+
+export function askMove() {
+  let row;
+  let column;
+  while (!["0", "1", "2"].includes(row)) {
+    row = prompt("What row do you choose ?");
+  }
+  while (!["0", "1", "2"].includes(column)) {
+    column = prompt("What column do you choose ?");
+  }
+  return [row, column];
+}
