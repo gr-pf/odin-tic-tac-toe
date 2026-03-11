cec150561d96d6847e95a86964e7087233c110b5
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-11 20:12:04 +0100

    Add submit form event

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 import { Game } from "./js/game.js";
+import { cleanCell } from "./functions-dom.js";
 
+const cells = document.querySelectorAll(".cell");
 const metaGame = new Game();
 // window.currentMetaGame = metaGame;
 // window.currentMetaGame.initGame();
 // window.currentMetaGame.playGame();
 //metaGame.initGame();
 //metaGame.playGame();
+
+// const newGameBtn = document.querySelector(".new-game-btn");
+// newGameBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const obj = Object.fromEntries(formData);
+    console.log(obj);
+    cells.forEach((cell) => cleanCell(cell));
+  });
+});
