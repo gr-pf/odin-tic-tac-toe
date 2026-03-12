230d4dafc648cded6722fcff7d3887f5c44cae7f
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-12 07:43:29 +0100

    Update main logic

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 import { Game } from "./js/game.js";
-import { cleanCell } from "./functions-dom.js";
+import { cleanCell, clickCell } from "./functions-dom.js";
 
 const cells = document.querySelectorAll(".cell");
-const metaGame = new Game();
-// window.currentMetaGame = metaGame;
-// window.currentMetaGame.initGame();
-// window.currentMetaGame.playGame();
-//metaGame.initGame();
-//metaGame.playGame();
-
-// const newGameBtn = document.querySelector(".new-game-btn");
-// newGameBtn.addEventListener("click", (e) => {
-//   e.preventDefault();
-// });
+let currentGame;
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("#form");
@@ -20,7 +10,14 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault();
     const formData = new FormData(form);
     const obj = Object.fromEntries(formData);
-    console.log(obj);
     cells.forEach((cell) => cleanCell(cell));
+    currentGame = new Game(obj);
+    currentGame.initGame();
+    window.currentMetaGame = currentGame;
   });
+});
+
+//Il manque l'event listener - clickCell(cell, currentGame)
+cells.forEach((cell) => {
+  cell.addEventListener("click", (event) => clickCell(cell, currentGame));
 });
