a5a71eedd41c59c5a4613326bb0b1aa483153a61
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-12 07:43:05 +0100

    Continue click cell

--- functions-dom.js
+++ functions-dom.js
@@ -1,3 +1,5 @@
+import { Game } from "./js/game.js";
+
 /**
  *
  * @param {HTMLElement} cell
@@ -5,4 +7,25 @@
 export function cleanCell(cell) {
   cell.innerText = "";
   cell.setAttribute("fill", "empty");
+}
+
+/**
+ *
+ * @param {HTMLElement} cell
+ * @param {Game} game
+ */
+export function clickCell(cell, Game) {
+  if (Game?.state !== "active") {
+    alert("You must init the game by completing the form!");
+    return;
+  }
+  if (cell.getAttribute("fill") === "full") {
+    alert("Cell is already used! Choose another one!");
+    return;
+  }
+  if (cell.getAttribute("fill") === "empty") {
+    cell.setAttribute("fill", "full");
+    cell.innerText = Game.playerTurn.mark;
+    Game.changeTurn();
+  }
 }
